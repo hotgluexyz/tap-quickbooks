cf4848f2ed72575ee92d828234d7af06e0038a9d
Álvaro Valarezo <alvaro@hotglue.io>
2023-05-11 11:56:58 -0500

    add support for full sync for gl

--- tap_quickbooks/quickbooks/__init__.py
+++ tap_quickbooks/quickbooks/__init__.py
@@ -227,11 +227,11 @@ class Quickbooks():
                  default_start_date=None,
                  api_type=None,
                  report_period_days = None,
-                 report_period_gl = None,
+                 gl_full_sync = None,
                  realm_id=None):
         self.api_type = api_type.upper() if api_type else None
         self.report_period_days = report_period_days
-        self.report_period_gl = report_period_gl
+        self.gl_full_sync = gl_full_sync
         self.include_deleted = include_deleted
         self.realm_id = realm_id
         self.refresh_token = refresh_token
