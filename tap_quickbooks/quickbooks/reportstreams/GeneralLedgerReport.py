ea8860eda4513051c86dd6630c6cf9fd691b2acf
Key <keynarafaelp@gmail.com>
2023-10-02 17:04:39 -0500

    format file

--- tap_quickbooks/quickbooks/reportstreams/GeneralLedgerReport.py
+++ tap_quickbooks/quickbooks/reportstreams/GeneralLedgerReport.py
@@ -14,9 +14,10 @@ from calendar import monthrange
 LOGGER = singer.get_logger()
 NUMBER_OF_PERIODS = 3
 
+
 class GeneralLedgerReport(QuickbooksStream):
     key_properties: ClassVar[List[str]] = []
-    replication_method: ClassVar[str] = 'FULL_TABLE'
+    replication_method: ClassVar[str] = "FULL_TABLE"
     gl_weekly = False
     gl_daily = False
 
@@ -37,7 +38,7 @@ class GeneralLedgerReport(QuickbooksStream):
 
     def _recursive_row_search(self, row, output, categories):
         row_group = row.get("Rows")
-        if 'ColData' in list(row.keys()):
+        if "ColData" in list(row.keys()):
             # Write the row
             data = row.get("ColData")
             values = [column for column in data]
@@ -56,7 +57,7 @@ class GeneralLedgerReport(QuickbooksStream):
                 self._recursive_row_search(row, output, categories)
             if header is not None:
                 categories.pop()
-        
+
     def clean_row(self, output, columns):
         # Zip columns and row data.
         for raw_row in output:
@@ -80,14 +81,22 @@ class GeneralLedgerReport(QuickbooksStream):
                 else:
                     cleansed_row.update({k: v})
 
-            cleansed_row["SyncTimestampUtc"] = singer.utils.strftime(singer.utils.now(), "%Y-%m-%dT%H:%M:%SZ")
+            cleansed_row["SyncTimestampUtc"] = singer.utils.strftime(
+                singer.utils.now(), "%Y-%m-%dT%H:%M:%SZ"
+            )
 
             yield cleansed_row
-    
+
     def concurrent_get(self, report_entity, params):
         response = self._get(report_entity, params)
-        if "Unable to display more data. Please reduce the date range." in str(response):
-            return {"error": "Too much data for current period", "start_date": params["start_date"], "end_date":params["end_date"]}
+        if "Unable to display more data. Please reduce the date range." in str(
+            response
+        ):
+            return {
+                "error": "Too much data for current period",
+                "start_date": params["start_date"],
+                "end_date": params["end_date"],
+            }
         else:
             return response
 
@@ -152,12 +161,11 @@ class GeneralLedgerReport(QuickbooksStream):
                 "subt_nat_home_amount",
                 "subt_nat_amount_home_nt",
             ]
-        
+
         params = {
             "accounting_method": self.accounting_method,
-            "columns": ",".join(cols)
+            "columns": ",".join(cols),
         }
-
 
         if full_sync or self.qb.gl_full_sync:
             LOGGER.info(f"Starting full sync of GeneralLedgerReport")
@@ -179,9 +187,9 @@ class GeneralLedgerReport(QuickbooksStream):
                 elif self.qb.gl_weekly or self.gl_weekly:
                     period_days = 7
                     max_requests = 4
-                else:   
+                else:
                     day1, period_days = monthrange(start_date.year, start_date.month)
-                    max_requests = 1   
+                    max_requests = 1
 
                 # calculate end date
                 if (today - start_date).days <= period_days:
@@ -189,7 +197,9 @@ class GeneralLedgerReport(QuickbooksStream):
                     params["end_date"] = today.strftime("%Y-%m-%d")
                 else:
                     end_date = start_date + relativedelta(days=+period_days)
-                    params["end_date"] = (end_date - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+                    params["end_date"] = (
+                        end_date - datetime.timedelta(days=1)
+                    ).strftime("%Y-%m-%d")
 
                 params["start_date"] = (start_date).strftime("%Y-%m-%d")
                 requests_params.append(params.copy())
@@ -201,15 +211,29 @@ class GeneralLedgerReport(QuickbooksStream):
                 if len(requests_params) < max_requests:
                     continue
                 else:
-                    [LOGGER.info(f"Fetch GeneralLedgerReport for period {x['start_date']} to {x['end_date']}") for x in requests_params]
-                    with concurrent.futures.ThreadPoolExecutor(max_workers=max_requests) as executor:
-                        resp = executor.map(lambda x: self.concurrent_get(report_entity='GeneralLedger', params=x), requests_params)
+                    [
+                        LOGGER.info(
+                            f"Fetch GeneralLedgerReport for period {x['start_date']} to {x['end_date']}"
+                        )
+                        for x in requests_params
+                    ]
+                    with concurrent.futures.ThreadPoolExecutor(
+                        max_workers=max_requests
+                    ) as executor:
+                        resp = executor.map(
+                            lambda x: self.concurrent_get(
+                                report_entity="GeneralLedger", params=x
+                            ),
+                            requests_params,
+                        )
                     requests_params = []
 
                 # parse data and set the new start_date
                 for r in resp:
                     if r.get("error") == "Too much data for current period":
-                        start_date = datetime.datetime.strptime(r.get("start_date"), "%Y-%m-%d")
+                        start_date = datetime.datetime.strptime(
+                            r.get("start_date"), "%Y-%m-%d"
+                        )
                         if not self.gl_weekly and not self.gl_daily:
                             self.gl_weekly = True
                         elif self.gl_weekly and not self.gl_daily:
@@ -219,7 +243,7 @@ class GeneralLedgerReport(QuickbooksStream):
                     else:
                         self.gl_weekly = False
                         self.gl_daily = False
-                        
+
                         # Get column metadata.
                         columns = self._get_column_metadata(r)
 
@@ -238,17 +262,21 @@ class GeneralLedgerReport(QuickbooksStream):
 
                         yield from self.clean_row(output, columns)
         else:
-            LOGGER.info(f"Syncing GeneralLedgerReport of last {NUMBER_OF_PERIODS} periods")
+            LOGGER.info(
+                f"Syncing GeneralLedgerReport of last {NUMBER_OF_PERIODS} periods"
+            )
             end_date = datetime.date.today()
 
             for i in range(NUMBER_OF_PERIODS):
                 start_date = end_date.replace(day=1)
 
-                params["start_date"] = end_date.replace(day=1).strftime("%Y-%m-%d"),
-                params["end_date"] = end_date.strftime("%Y-%m-%d"),
+                params["start_date"] = (end_date.replace(day=1).strftime("%Y-%m-%d"),)
+                params["end_date"] = (end_date.strftime("%Y-%m-%d"),)
 
-                LOGGER.info(f"Fetch GeneralLedgerReport for period {params['start_date']} to {params['end_date']}")
-                resp = self._get(report_entity='GeneralLedger', params=params)
+                LOGGER.info(
+                    f"Fetch GeneralLedgerReport for period {params['start_date']} to {params['end_date']}"
+                )
+                resp = self._get(report_entity="GeneralLedger", params=params)
 
                 # Get column metadata.
                 columns = self._get_column_metadata(resp)
