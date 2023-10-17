f4c959f8f507a3431e65255fd525c259bda68b95
Key <keynarafaelp@gmail.com>
2023-10-17 13:19:44 -0500

    remove value from custom_field

--- tap_quickbooks/quickbooks/__init__.py
+++ tap_quickbooks/quickbooks/__init__.py
@@ -140,7 +140,6 @@ def field_to_property_schema(field, mdata):  # pylint:disable=too-many-branches
         "type": object_type["type"],
         "properties": {
             "DefinitionId": string_type,
-            "value": string_type,
             "Name": string_type,
             "Type": string_type,
             "StringValue": string_type
