77e67792aa8d5cba35f3e4c7db2499ba8982a757
Jong Lee <76450939+Jojo970@users.noreply.github.com>
2023-11-13 11:52:21 -0500

    HGI-4546 (#77)
    Co-authored-by: Jong <jonghotglue@jongs-mbp.mynetworksettings.com>

--- tap_quickbooks/quickbooks/rest.py
+++ tap_quickbooks/quickbooks/rest.py
@@ -105,7 +105,7 @@ class Rest():
             headers.update(self.qb._get_standard_headers())
             records_deleted = []
             excluded_entities = ["Bill", "Payment", "Transfer", "CompanyInfo", "CreditMemo", "Invoice",
-                                 "JournalEntry", "Preferences", "Purchase", "SalesReceipt", "TimeActivity"]
+                                 "JournalEntry", "Preferences", "Purchase", "SalesReceipt", "TimeActivity", "BillPayment"]
             if self.qb.include_deleted and stream not in excluded_entities:
                 # Get the deleted records first
                 if "WHERE" in query:
