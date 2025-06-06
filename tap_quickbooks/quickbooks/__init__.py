1f74c4fe7adbedea1fd5960343c3e54cf4448bcf
Davi Souza <ddas.souza@gmail.com>
2025-06-06 13:33:16 -0300

    HG-4215: saves every request (not only GET)

--- tap_quickbooks/quickbooks/__init__.py
+++ tap_quickbooks/quickbooks/__init__.py
@@ -413,14 +413,13 @@ class Quickbooks():
             raise TapQuickbooksException("Unsupported HTTP method")
 
         try:
-            if http_method in ["GET"]:
-                save_api_usage(
-                    http_method,
-                    url,
-                    params,
-                    body,
-                    resp
-                )
+            save_api_usage(
+                http_method,
+                url,
+                params,
+                body,
+                resp
+            )
         except Exception as e:
             LOGGER.error("Error saving API usage: %s", str(e))
 
