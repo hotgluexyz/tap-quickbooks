9a9e37396aea5d74e02f60145327e6318bcb657f
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2025-07-15 14:29:09 +0500

    hgi-5302: add report_periods check for full sync

--- tap_quickbooks/quickbooks/reportstreams/TransactionListReport.py
+++ tap_quickbooks/quickbooks/reportstreams/TransactionListReport.py
@@ -48,7 +48,7 @@ class TransactionListReport(BaseReportStream):
                 categories.pop()
 
     def sync(self, catalog_entry):
-        full_sync = not self.state_passed
+        full_sync = not self.state_passed and self.number_of_periods is None
 
         if full_sync:
             LOGGER.info(f"Starting full sync of TransactionListReport")
