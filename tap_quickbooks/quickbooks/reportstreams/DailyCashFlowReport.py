89e2506897dc9d04438302b9b685f4dd8620733f
Key <keynarafaelp@gmail.com>
2025-08-21 10:52:27 -0500

    make all reports but GLR and PNL full sync

--- tap_quickbooks/quickbooks/reportstreams/DailyCashFlowReport.py
+++ tap_quickbooks/quickbooks/reportstreams/DailyCashFlowReport.py
@@ -76,127 +76,74 @@ class DailyCashFlowReport(BaseReportStream):
         if end_date <= start_date:
             end_date = start_date
         return end_date
+
     def sync(self, catalog_entry):
-        full_sync = not self.state_passed and not self.has_number_of_periods
-
-        if full_sync or self.qb.report_period_days:
-            LOGGER.info(f"Starting full sync of CashFlow")
-            current_date = datetime.datetime.now() - timedelta(days=1)
-            #getting today's date as a datetime to avoid type errors
-            min_time = datetime.datetime.min.time()
-            today = datetime.date.today()
-            today_datetime = datetime.datetime.combine(today, min_time)
-            #-
-            end_date = today_datetime + datetime.timedelta(60)
-            if self.qb.report_period_days:
-                start_date = today_datetime - datetime.timedelta(int(self.qb.report_period_days))
-            else:
-                start_date = self.start_date
-            start_date = start_date.replace(tzinfo=None)
-
-            if self.check_date_greater_than_months(start_date):
-                end_date = self.add_months(start_date)
-            end_date = self.correct_end_date(end_date,start_date,current_date)    
-            params = {
-                "start_date": start_date.strftime("%Y-%m-%d"),
-                "end_date": end_date.strftime("%Y-%m-%d"),
-                "accounting_method": "Accrual",
-                "summarize_column_by": "Days"
-            }
-            
-            while start_date.replace(tzinfo=None) <= current_date:
-                LOGGER.info(f"Fetch DailyCashFlow Report for period {params['start_date']} to {params['end_date']}")
-                resp = self._get(report_entity='CashFlow', params=params)
-
-                # Get column metadata.
-                columns = self._get_column_metadata(resp)
-
-                # Recursively get row data.
-                row_group = resp.get("Rows")
-                row_array = row_group.get("Row")
-
-                if row_array is None:
-                    return
-
-                output = []
-                categories = []
-                for row in row_array:
-                    self._recursive_row_search(row, output, categories)
-
-                # Zip columns and row data.
-                for raw_row in output:
-                    row = dict(zip(columns, raw_row))
-                    if not row.get("Total"):
-                        # If a row is missing the amount, skip it
-                        continue
-
-                    cleansed_row = {}
-                    for k, v in row.items():
-                        if v == "":
-                            continue
-                        else:
-                            cleansed_row.update({k: v})
-
-                    cleansed_row["Total"] = float(row.get("Total"))
-                    cleansed_row["SyncTimestampUtc"] = singer.utils.strftime(singer.utils.now(), "%Y-%m-%dT%H:%M:%SZ")
-                    daily_total = []
-                    for key,value in cleansed_row.items():
-                        if key not in ['Account', 'Categories', 'SyncTimestampUtc', 'Total']:
-                            daily_total.append({key:value})
-                    cleansed_row['DailyTotal'] = daily_total
-                    start_date = end_date + timedelta(days=1)
-                    end_date = self.add_months(start_date)
-                    end_date = self.correct_end_date(end_date,start_date,current_date)  
-                    yield cleansed_row
+        LOGGER.info(f"Starting full sync of CashFlow")
+        current_date = datetime.datetime.now() - timedelta(days=1)
+        #getting today's date as a datetime to avoid type errors
+        min_time = datetime.datetime.min.time()
+        today = datetime.date.today()
+        today_datetime = datetime.datetime.combine(today, min_time)
+        #-
+        end_date = today_datetime + datetime.timedelta(60)
+        if self.qb.report_period_days:
+            start_date = today_datetime - datetime.timedelta(int(self.qb.report_period_days))
         else:
-            LOGGER.info(f"Syncing CashFlow of last {self.number_of_periods} periods")
-            end_date = datetime.date.today()
+            start_date = self.start_date
+        start_date = start_date.replace(tzinfo=None)
 
-            for i in range(self.number_of_periods):
-                start_date = end_date.replace(day=1)
-                params = {
-                    "start_date": start_date.strftime("%Y-%m-%d"),
-                    "end_date": end_date.strftime("%Y-%m-%d"),
-                    "accounting_method": "Accrual"
-                }
+        if self.check_date_greater_than_months(start_date):
+            end_date = self.add_months(start_date)
+        end_date = self.correct_end_date(end_date,start_date,current_date)    
+        params = {
+            "start_date": start_date.strftime("%Y-%m-%d"),
+            "end_date": end_date.strftime("%Y-%m-%d"),
+            "accounting_method": "Accrual",
+            "summarize_column_by": "Days"
+        }
+        
+        while start_date.replace(tzinfo=None) <= current_date:
+            LOGGER.info(f"Fetch DailyCashFlow Report for period {params['start_date']} to {params['end_date']}")
+            resp = self._get(report_entity='CashFlow', params=params)
 
-                LOGGER.info(f"Fetch CashFlow for period {params['start_date']} to {params['end_date']}")
-                resp = self._get(report_entity='CashFlow', params=params)
+            # Get column metadata.
+            columns = self._get_column_metadata(resp)
 
-                # Get column metadata.
-                columns = self._get_column_metadata(resp)
+            # Recursively get row data.
+            row_group = resp.get("Rows")
+            row_array = row_group.get("Row")
 
-                # Recursively get row data.
-                row_group = resp.get("Rows")
-                row_array = row_group.get("Row")
+            if row_array is None:
+                return
 
-                if row_array is None:
-                    # Update end date
-                    end_date = start_date - datetime.timedelta(days=1)
+            output = []
+            categories = []
+            for row in row_array:
+                self._recursive_row_search(row, output, categories)
+
+            # Zip columns and row data.
+            for raw_row in output:
+                row = dict(zip(columns, raw_row))
+                if not row.get("Total"):
+                    # If a row is missing the amount, skip it
                     continue
 
-                output = []
-                categories = []
-                for row in row_array:
-                    self._recursive_row_search(row, output, categories)
-
-                # Zip columns and row data.
-                for raw_row in output:
-                    row = dict(zip(columns, raw_row))
-                    if not row.get("Total"):
-                        # If a row is missing the amount, skip it
+                cleansed_row = {}
+                for k, v in row.items():
+                    if v == "":
                         continue
+                    else:
+                        cleansed_row.update({k: v})
 
-                    cleansed_row = {}
-                    for k, v in row.items():
-                        if v == "":
-                            continue
-                        else:
-                            cleansed_row.update({k: v})
-
-                    cleansed_row["Total"] = float(row.get("Total"))
-                    cleansed_row["SyncTimestampUtc"] = singer.utils.strftime(singer.utils.now(), "%Y-%m-%dT%H:%M:%SZ")
-
-                    yield cleansed_row
-
-                end_date = start_date - datetime.timedelta(days=1)
+                cleansed_row["Total"] = float(row.get("Total"))
+                cleansed_row["SyncTimestampUtc"] = singer.utils.strftime(singer.utils.now(), "%Y-%m-%dT%H:%M:%SZ")
+                daily_total = []
+                for key,value in cleansed_row.items():
+                    if key not in ['Account', 'Categories', 'SyncTimestampUtc', 'Total']:
+                        daily_total.append({key:value})
+                cleansed_row['DailyTotal'] = daily_total
+                start_date = end_date + timedelta(days=1)
+                end_date = self.add_months(start_date)
+                end_date = self.correct_end_date(end_date,start_date,current_date)  
+                yield cleansed_row
+ 
